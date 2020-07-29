21e004348184c85976a1d72827cc97045fcf0a6f
Greg Douglas <greg@storyblocks.com>
2020-07-29 17:49:31 -0400

    It's-a me, Mario!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import * as PIXI from 'pixi.js';
+import marioSprite from './images/mario.png';
 
-let type = 'WebGL';
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas';
-}
+const root = document.getElementById('root');
 
-PIXI.utils.sayHello(type);
+let app = new PIXI.Application({
+  antialias: true,
+  backgroundColor: 0x9193f8,
+  resolution: 1,
+});
+
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const texture = PIXI.Texture.from(marioSprite);
+const mario = new PIXI.Sprite(texture);
+
+container.addChild(mario);
+
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+app.ticker.add((delta) => {
+  container.rotation -= 0.01 * delta;
+});
+
+root.appendChild(app.view);
